d6922c961eba86b2a0bdc9abf213640b81e07ab6
FathimaSaloojaA <fathimasaloojaa@gmail.com>
2025-11-15 21:08:07 +0530

    removed urlparser

--- server.js
+++ server.js
@@ -28,10 +28,11 @@ app.use('/api/report', reportRoutes);
 
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => {
     console.error('MongoDB connection error', err);
   });
+
